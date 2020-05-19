5558c79354ae8c9d0a831e53479997b3b63f98b3
tugonbob <57576522+tugonbob@users.noreply.github.com>
2020-05-19 16:32:52 -0500

    Bug fixed print functions, added more chars to char_dict

--- LedMatrix.py
+++ LedMatrix.py
@@ -1,6 +1,5 @@
 '''
 Code for for an LED matrix
-
 # 32 x 8 led matrix. Works like a snaking led strip
 # index of led strip
 # 0 15 16 31 32 47 48 63 64 79 80 95 96  111 112 127 128 143 144 159 160 175 176 191 192 207 208 223 224 239 240 255
@@ -13,12 +12,11 @@ Code for for an LED matrix
 # 7 8  23 24 39 40 55 56 71 72 87 88 103 104 119 120 135 136 151 152 167 168 183 184 199 200 215 216 231 232 247 248
 '''
 
-#from rpi_ws281x import *
-
 import neopixel
 import board
 import time
 from characterDictionary import char_dict
+from CircularQueueClass import CircularQueue
 
 pixel_pin = board.D18
 num_pixels = 256
@@ -38,20 +36,19 @@ PINK = (255,0,100)
 
 #global variables
 cursorx = 0
-cursory = 1
-COLOR = RED
+cursory = 0
 
 #constants
 MATRIX_HEIGHT = 8
 MATRIX_LENGTH = 32
-LEFT_ALIGN = "LEFT"
-CENTER_ALIGN = "CENTER"
-CHAR_LIST = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890:'
+LEFT = "LEFT"
+CENTER = "CENTER"
+CHAR_LIST = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 ?!&.,()_:;% @#$^*+-=[]{}|\'\"/<>~'
 
 # converts a ledstrip index to a coord
 def coord_of(pos):
     x = int(pos // 8)
-    if(x % 2 == 0):
+    if (x % 2 == 0):
         y = int(pos % 8)
     else:
         y = int(8 - (pos % 8))
@@ -69,9 +66,9 @@ def pixel_length(str):
     total_length = 0
 
     for char in str:
-        if char in CHAR_LIST:
+        if(char in CHAR_LIST):
             char_length = len(char_dict[char][0])
-            total_length += char_length + 1
+            total_length += char_length
     return total_length - 1
 
 # color wheel
@@ -97,51 +94,46 @@ def wheel(pos):
     return (r, g, b)
 
 # print a single character
-def print_char(char):
+def print_char(char, color):
     global cursorx
-
     char_height = len(char_dict[char])
     char_length = len(char_dict[char][0])
-
+    
     for i in range(0, char_height): # loop y
         for j in range(0, char_length): # loop x
             if(cursorx + j < MATRIX_LENGTH and cursorx + j >= 0): # check x bounds
                 if(cursory + i < MATRIX_HEIGHT and cursory + i >= 0): # check y bounds
                     if(char_dict[char][i][j] == 1):
-                        strip[index_of(cursorx + j,cursory + i)] = COLOR
+                        strip[index_of(cursorx + j,cursory + i)] = color
                     else:
                         strip[index_of(cursorx + j, cursory + i)] = OFF # erase led's that are ON from previous print
     cursorx = cursorx + char_length #move cursorx
 
 # print str at a specific x and y
-def print_str_loc(str, color = None, x, y = None):
+def print_str(str, color, x, y = None):
     global cursorx, cursory
     cursorx = x
     if(y is not None):
         cursory = y
 
-    if(color is not None):
-        global COLOR
-        COLOR = color
-
     for char in str:
         if(char in CHAR_LIST):
-            print_char(char)
+            print_char(char, color)
         else:
-            print_char('null')
+            print_char('null', color)
     strip.show()
 
 # print a string using given alignment
-def print_str(str, color = None, align = None):
-    if(align == LEFT_ALIGN):
+def align_print(str, color, align):
+    if(align == LEFT):
         # change cursorx to reflect alignment
         x = 0
-        print_str_loc(str, color, x)
+        print_str(str, color, x, 0)
     else:
         # use CENTER_ALIGN
         pixel_len = pixel_length(str)
         x = int((MATRIX_LENGTH - pixel_len) / 2) # find equal distance between edges and str
-        print_str_loc(str, color, x)
+        print_str(str, color, x, 0)
 
 # print str, scrolling through colors
 def rainbow_print(str):
@@ -149,26 +141,20 @@ def rainbow_print(str):
     global cursory
 
     while True:
-        for degree in range(1, 255):
-            cursorx, cursory = (0,1)
-            print_str(str, wheel(degree))
+        for color in range(1, 255):
+            align_print(str, wheel(color), CENTER)
 
 # scrolling print
 # todo: loop the text
-def marquee_print(str, color = None, speed = None):
-    if(color is not None):
-        global COLOR
-        COLOR = color
-
+def marquee_print(str, color, speed = None):
     if(speed is None):
         speed = .04
-
-    for x in range(32, 0 - pixel_length(str), -1):
-        print_str_loc(str, COLOR, x, cursory)
+    for x in range(32, -1 - pixel_length(str), -1): #decrement x till off matrix edge
+        print_str(str, color, x, None)
         time.sleep(speed)
 
 # Main method
 if(__name__ == "__main__"):
-    marquee_print("HELLO", None, .01)
     if(cursorx < MATRIX_LENGTH and cursorx > 0):
         strip[index_of(cursorx, cursory)] = BLUE
+    strip.show()
